4c14a0b99c0fb958c5c71f7fd376b24ac4bd44c8
Thuan Chau <chauduthuan@gmail.com>
2018-06-13 01:57:26 -0700

    add functions to patch tree problem: iteration to create schema run before patch tree

--- XmlTree.py
+++ XmlTree.py
@@ -21,14 +21,17 @@ class XmlTree:
         self.schema["name"] = self.file_name
         self.schema["children"] = []
         self.schema_root = self.schema["children"]
-        
         self.initialize_parent_map()
         self.initialize_namespace_map()
-        self.breath_first_iteration()
+        
         #SOME NOTE
         #before createing schema, look up all dependencies such as base, ref or xs:include
         #add dependencies to a dictionary
         #modify tree by adding these dependencies
+        self.dependencies = {} #dictionary for dependencies, key is name, value is element tree        
+        self.initialize_dependencies_and_patch_tree()
+        
+        #self.breath_first_iteration()
 
 
     def initialize_parent_map(self):
@@ -51,6 +54,98 @@ class XmlTree:
                 namespaces["{" + elem[1] ] = elem[0]
         self.namespace_map = namespaces
         print(self.namespace_map)
+        
+    def initialize_dependencies_and_patch_tree(self):
+        """create dependencies dictionary which key is name and key is elementree
+        example: "Deployment":element 
+        then patch all missing element in tree, for example, xs:extension 
+        """
+        ##append dependencies
+        #append denendencies in xs:include
+        file_names = self.get_included_dependencies_names()
+        for file_name in file_names:
+            xmlTree = XmlTree(file_name)
+            included_dependencies = xmlTree.get_dependencies()
+            for key in included_dependencies:
+                #probably need to check for duplicate keys
+                self.dependencies[key] = included_dependencies[key]
+        
+        #append dependencies in current file
+        for node in self.root.getchildren():
+            tag = self.get_node_tag(node)
+            attributes = node.attrib
+            if "name" in attributes:
+                name = attributes["name"]
+                self.dependencies[name] = node
+            
+        #patch tree
+        print("FINDING NODES NEED PATCH")
+        nodes_need_patch = self.find_node_need_patches()
+        for node in nodes_need_patch:
+            print(self.get_node_tag(node))
+            name = self.get_base_or_ref(node)
+            print("base = " + name)
+            node.insert(0, self.dependencies[name])
+            
+
+                    
+        
+    def find_node_need_patches(self):
+        """Iterate through tree, append node that is needed to patch up
+        return list of nodes/element 
+        """
+        nodes_need_patch = []
+        for node in self.root.getiterator():
+            if self.is_node_needed_to_be_patched(node):
+                nodes_need_patch.append(node)
+        return nodes_need_patch
+    
+    def is_node_needed_to_be_patched(self, node):
+        """check if node needs to be patched
+        return true if node is xs:extension or has attribute base
+        """
+        #tag = self.get_node_tag(node)
+        attributes = node.attrib
+        if ("ref" in attributes):
+            ref = attributes["ref"]
+            if not self.is_xs_reference(ref):
+                return True
+        elif ("base" in attributes):
+            base = attributes["base"]
+            if not self.is_xs_reference(base):
+                return True
+        return False
+    
+    def is_xs_reference(self, ref):
+        """check if reference is xs type such as xs:string """
+        xs = ref[0:2]
+        return xs=="xs"
+    
+    def get_base_or_ref(self, node):
+        attributes = node.attrib
+        name = ""
+        if ("ref" in attributes):
+             name = attributes["ref"]
+        elif ("base" in attributes):
+            name = attributes["base"]
+        return name
+    
+    def get_dependencies(self):
+        """return dictionary of dependencies"""
+        return self.dependencies
+    
+    def get_included_dependencies_names(self):
+        """return list of file_name in xs:include 
+        <xs:include schemaLocation="CommonElements.xsd"/>
+        """
+        dependencies_names = []
+        for node in self.root.getchildren():
+            tag = self.get_node_tag(node)
+            if tag == "xs:include":
+                attributes = node.attrib
+                schema_location = attributes["schemaLocation"]
+                dependencies_names.append(schema_location)
+        return dependencies_names
 
 
     def get_node_parent(self, node):
@@ -85,7 +180,7 @@ class XmlTree:
             if parent is None:
                 break
             parent_list.append(parent)
-     
+        #reverse the list to have the order correctly, first element is root
         parent_list.reverse()
         return parent_list
     
@@ -121,10 +216,9 @@ class XmlTree:
         #NEED to ADD root to schema, root can have multiple childs and root is xs:schema
         #if (((tag == "xs:element") or (tag == "xs:complexType"))and (name!="")):
         if (name != ""):
-            print("tag = " + tag)
-            print("name = " + name)
+            #self.print_tag_and_name_of_node(node)
+            #self.print_parent_name_list(parent_name_list)
             
-            self.print_parent_name_list(parent_name_list)
             # do stuff such as adding node to schema
             schema_node = defaultdict(dict)
             schema_node["name"] = name
@@ -160,7 +254,17 @@ class XmlTree:
         for name in parent_name_list:
             print(name)
         print("=============")
-    
+        
+    def print_tag_and_name_of_node(self, node):
+        tag = self.get_node_tag(node)
+        attributes = node.attrib
+        name = ""
+        if "name" in attributes:
+            name = attributes["name"]
+
+            print("tag = " + tag)
+            print("name = " + name)
+        
     def get_node_tag(self, node):
         """
         node.tag contains {uri}type such as {http://www.w3.org/2001/XMLSchema}element, not xs:element, xs:schema
@@ -178,10 +282,21 @@ class XmlTree:
     
 if __name__ == '__main__':
     print("*****Start*****")
-    file_name = 'ex1.xsd'
+    file_name = 'Deployment.xsd'
     xmlTree = XmlTree(file_name)
+    xmlTree.breath_first_iteration()
     schema = xmlTree.get_schema()
     schema = json.dumps(schema, indent=4)
-    print(schema)
+    #print(schema)
+    
+    dependencies_names = xmlTree.get_included_dependencies_names()
+    print(dependencies_names)
+    
+    dependencies = xmlTree.get_dependencies()
+    for key in dependencies:
+        print(key)
+        
+        
+    
     
     print("*****End*****")
--- xsd_to_json.py
+++ xsd_to_json.py
@@ -1,8 +1,10 @@
 import xml.etree.ElementTree as ET
 import json
 from XmlTree import XmlTree
+import xml.dom.minidom as minidom
 
-file_name = "book.xml"
+
+file_name = "ex2.xsd"
 #file_name = "sampleXSD.xsd"
 #file_name = "Deployment.xsd"
 
@@ -35,23 +37,60 @@ seems cannot keep track of parents
 
 	
 """ Depth first search Iteration """
-parent_map = parent_map = {c:p for p in tree.iter() for c in p}
+#parent_map = parent_map = {c:p for p in tree.iter() for c in p}
 
-for c in tree.iter():
-    if c == root:
-        continue
-    print(c.tag)
-    p = parent_map[c]
-    print(p.tag)
-    print("====================")
+#for c in tree.iter():
+#    if c == root:
+#        continue
+#    print(c.tag)
+#    p = parent_map[c]
+#    print(p.tag)
+#    print("====================")
+#    
+#js = {"a":{"b":"c"}}
+#print(js)
+#
+
+dependencies = {}
+for node in root.getchildren():
+    #tag = get_node_tag(node)
+    attributes = node.attrib
+    if "name" in attributes:
+        name = attributes["name"]
+        dependencies[name] = node    
+        
+for key in dependencies:
+    print(key)
+
+def node_contains_ref(node):
+    attributes = node.attrib
     
-js = {"a":{"b":"c"}}
-print(js)
+
+node_need_to_patch = []
+for node in root.getiterator():
+    attributes = node.attrib
+    if "ref" in attributes:
+        node_need_to_patch.append(node)
+
+dependencies = {}
+for node in root.getchildren():
+    attributes = node.attrib
+    if "name" in attributes:
+        name = attributes["name"]
+        dependencies[name] = node
+        
+for node in node_need_to_patch:
+    attrib = node.attrib
+    name = attrib["ref"]
+    node.insert(0, dependencies[name])
     
-    
-    
-    
-    
+#for node in node_need_to_patch:
+#    print("ref " + node.attrib["ref"])
+#    for child in node.getchildren():
+#        print("--->" + child.tag)
+#        print("--->" + child.attrib["name"])
+
+xmlstr = minidom.parseString(ET.tostring(root)).toprettyxml(indent="   ")
     
     
     
